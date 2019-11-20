25b4d571b3d728da24c1b2880b94167ca124c39c
Fabio Bumann <fabio.bumann3@lernende.bfo-vs.ch>
2019-11-20 12:42:13 +0100

    Add Multiply

--- index.php
+++ index.php
@@ -1,20 +1,36 @@
 <?php
-	$name = "";
-	if(isset($_GET['name'])){
+	$firstnumber = 0;
+	$secondnumber = 0;
+	$ergebnis = 0;
+	$name = "";	
+
+	if(isset($_POST['firstnumber'])){
+		$firstnumber = $_POST['firstnumber'];
+	}
+	if(isset($_POST['secondnumber'])){
+		$secondnumber = $_POST['secondnumber'];
+	}
+	if(isset($_POST['name'])){
 		$name = $_GET['name'];
 	}
+	
 ?>
+
 <html>
 <head>
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
-
 <body>
-	<?php
-		echo "<h1>Hallo ".$name."</h1>";
-	?>
-	<form>
-		<input type="text" name="name">
+<form method="POST">
+		<input type="text" name="firstnumber">
+		<input type="text" name="secondnumber">	
+		<input type="submit">
 	</form>
+
+	<?php
+		$ergebnis = $firstnumber * $secondnumber;
+		echo "<h1>Das Ergebnis ist:".$ergebnis."</h1>";	
+	?>
+
 </body>
 </html>
